e045d2e8eebcb17f256cbe2d28fda7c8a3b30a37
Bara Salameh <barasalameh90@gmail.com>
2021-08-13 18:03:34 +0300

    updaets on booker form and the mechanism

--- where-on-earth/src/components/BookerForm.js
+++ where-on-earth/src/components/BookerForm.js
@@ -12,11 +12,14 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
+
 const BookerForm = (props) => {
+
     const classes = useStyles();
     const {initialBookerName, initialBookerEmail, SCB } = props;
     const [bookerName, setBookerName] = useState(initialBookerName);
     const [bookerEmail, setBookerEmail] = useState(initialBookerEmail);
+    
     const onSubmitHandler = e => {
         e.preventDefault();
         SCB({ bookerName, bookerEmail });
@@ -25,15 +28,15 @@ const BookerForm = (props) => {
     return (
         <>
             <form onSubmit={onSubmitHandler} className={classes.root} noValidate autoComplete="off">
-                <TextField id="standard-basic" label="Full Name" onChange={(e) => { setBookerName(e.target.value) }}/>
+                <TextField id="standard-basic" label="Full Name" onChange={(e) => setBookerName(e.target.value) }/>
                 <br/>
-                <TextField id="standard-basic" label="Email" onChange={(e) => { setBookerEmail(e.target.value) }}/>
+                <TextField id="standard-basic" label="Email" onChange={(e) => setBookerEmail(e.target.value) }/>
                 <br/>
                 <br/>
                 <br/>
-                <RadioButtonsGroup/><br/>
+                <RadioButtonsGroup/>
+                <br/>
                 <Button type="submit" variant="contained" color="">Confirm</Button><br/>
-                
             </form>
             </>
     )
